d3b70969fbc9720a0306bc7571b464cd61ba8d29
hoangvudma <ngoduchoangvu.uel@gmail.com>
2018-10-15 08:57:19 +0700

    project

---
+++ demo/src/sample/Controller.java
@@ -0,0 +1,13 @@
+package sample;
+
+import javafx.event.ActionEvent;
+
+public class Controller{
+    public void pressStart(ActionEvent event){
+        System.out.println("hello world");
+    }
+    public void pressQuit(ActionEvent event){
+
+        System.out.println("hello world");
+    }
+}
